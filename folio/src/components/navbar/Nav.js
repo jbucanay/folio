0750293e94a9b67517ff6cca96987a3fd27f5fad
jbucanay <gilbert.baracka@gmail.com>
2021-11-09 00:30:59 -0700

    navi

--- folio/src/components/navbar/Nav.js
+++ folio/src/components/navbar/Nav.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import {Link} from 'react-router-dom'
 
-export default class Nav extends React.Component {
-    render(){
-        return (
-            <nav>
-               <a><Link to={'/'}>Home</Link></a>
-                <a><Link to={'/about'}>About </Link></a>
-            </nav>
-        )
-    }
+
+export default function Nav() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+
+          </IconButton>
+         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to={'/'}> Home</Link>
+          </Typography>
+             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to={'/about'}> About</Link>
+          </Typography>
+             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to={'/work'}> Work</Link>
+          </Typography>
+             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to={'/blog'}> Blog</Link>
+          </Typography>
+          <Button color="inherit"><Link to={'/contact'}>Contact</Link></Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }

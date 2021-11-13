8968f7bf849c7bd4027f6a57ec386baa07555ec1
jbucanay <gilbert.baracka@gmail.com>
2021-11-13 00:50:05 -0700

    form

--- folio/src/components/contact/Contact.js
+++ folio/src/components/contact/Contact.js
@@ -11,11 +11,21 @@ export default function Contact() {
             <h1 className='talk'>Let's talk.</h1>
                       <p className='contact'>Get in touch via the form below, or by emailing <a href={"mailto: test@test.com"} target={'_blank'} className='email'>test@test.com</a>.</p>
            </header>
-                <label>
+                <section className={'form'}>
+                <label className={'label'}>
                 Name:
-                <input type="text" name="name" />
+                <input type="text" name="name" placeholder={'Enter your name'}/>
                 </label>
-            <input type="submit" value="Submit" />
+                <label className={'label'}>
+                    Email Address:
+                    <input type={'email'} name={'email'} placeholder={'Enter your email address'} className={'infield'}/>
+                </label>
+                <label className={'label'}>
+                    Message:
+                    <textarea placeholder={'Enter your message'}/>
+                </label>
+                <input type='submit' value={'Submit'}/>
+</section>
             </form>
 
         )
